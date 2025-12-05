d5bbd5a5655f6d0b091015f22980d3a2dcebdafc
facebooklogin <hermanndag226@gmail.com>
2025-12-05 11:36:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def login():
     return "Merci !"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
